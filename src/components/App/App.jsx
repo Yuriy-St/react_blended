23c994c6a3140b54860bf39d8d2d6dc12a5767d5
VovaMyb <rimidalvatopayl@gmail.com>
2023-07-30 16:28:03 +0300

    Merge branch 'main' of https://github.com/Yuriy-St/react_blended

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,8 +8,8 @@ import {
   CryptoHistory,
 } from 'components';
 import data from 'data/data.json';
-
 import article from 'data/article.json';
+import forbes from 'data/forbes.json';
 
 export const App = () => {
   return (
@@ -37,7 +37,7 @@ export const App = () => {
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 3
         </Heading>
-        <ForbesList />
+        <ForbesList list={forbes} />
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 4
         </Heading>
