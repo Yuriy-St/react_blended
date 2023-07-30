872d7ba9bdc81647f0a19bebd73608506407622a
Yuriy-St <yuriystaynov@gmail.com>
2023-07-30 15:09:20 +0300

    task_1 completed

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,3 +1,6 @@
+import { formatDistanceToNow } from 'date-fns';
+import PropTypes from 'prop-types';
+
 import {
   Card,
   CardBody,
@@ -11,9 +14,47 @@ import {
   UserBox,
   UserInfo,
   UserName,
-  Date,
+  Date as DateEl,
 } from './BlogCard.styled';
 
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+export const BlogCard = ({
+  id,
+  poster,
+  tag,
+  title,
+  description,
+  userName,
+  avatar,
+  postedAt,
+}) => (
+  <Card data-id={id}>
+    <CardHeader>
+      <CardPoster src={poster} alt={tag} />
+    </CardHeader>
+    <CardBody>
+      <Tag>{tag}</Tag>
+      <CardTitle>{title}</CardTitle>
+      <CardText>{description}</CardText>
+    </CardBody>
+    <CardFooter>
+      <UserBox>
+        <Avatar src={avatar} alt={userName} />
+        <UserInfo>
+          <UserName>{userName}</UserName>
+          <DateEl>{formatDistanceToNow(new Date(postedAt))}</DateEl>
+        </UserInfo>
+      </UserBox>
+    </CardFooter>
+  </Card>
+);
+
+BlogCard.propTypes = {
+  id: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  userName: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  postedAt: PropTypes.string.isRequired,
 };
