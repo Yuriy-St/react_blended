fa6aa44a03c2810273f1f689ed65db840949a875
Yuriy-St <yuriystaynov@gmail.com>
2023-09-10 14:26:05 +0300

    new state

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,49 +1,88 @@
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
 import {
-  BlogCard,
   Container,
+  Grid,
+  GridItem,
+  Header,
+  SearchForm,
   Section,
-  Heading,
-  Statistics,
-  ForbesList,
-  CryptoHistory,
+  Text,
+  Todo,
 } from 'components';
-import data from 'data/data.json';
-import article from 'data/article.json';
-import forbes from 'data/forbes.json';
-import transactions from 'data/transactions.json';
 
-export const App = () => {
-  return (
-    <Section>
-      <Container>
-        <Heading marginBottom="50px" textAlign="center">
-          Task 1
-        </Heading>
-        <BlogCard
-          id={article._id}
-          poster={article.poster}
-          tag={article.tag}
-          title={article.title}
-          description={article.description}
-          userName={article.name}
-          avatar={article.avatar}
-          postedAt={article.postedAt}
-        />
-        <Heading marginTop="50px" marginBottom="50px" textAlign="center">
-          Task 2
-        </Heading>
-        <Statistics stats={data} />
-        <Statistics title="Main Statistics" stats={data} />
+export class App extends Component {
+  state = {
+    todos: [],
+  };
 
-        <Heading marginTop="50px" marginBottom="50px" textAlign="center">
-          Task 3
-        </Heading>
-        <ForbesList list={forbes} />
-        <Heading marginTop="50px" marginBottom="50px" textAlign="center">
-          Task 4
-        </Heading>
-        <CryptoHistory items={transactions} />
-      </Container>
-    </Section>
-  );
-};
+  componentDidMount() {
+    const todos = JSON.parse(localStorage.getItem('todos'));
+
+    if (todos) {
+      this.setState(() => ({ todos }));
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    const { todos } = this.state;
+
+    if (prevState.todos !== todos) {
+      localStorage.setItem('todos', JSON.stringify(todos));
+    }
+  }
+
+  addTodo = text => {
+    const todo = {
+      id: nanoid(),
+      text,
+    };
+
+    this.setState(({ todos }) => ({
+      todos: [...todos, todo],
+    }));
+  };
+
+  handleSubmit = data => {
+    this.addTodo(data);
+  };
+
+  deleteTodo = id => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== id),
+    }));
+  };
+
+  render() {
+    const { todos } = this.state;
+
+    return (
+      <>
+        <Header />
+        <Section>
+          <Container>
+            <SearchForm onSubmit={this.handleSubmit} />
+
+            {todos.length === 0 && (
+              <Text textAlign="center">There are no any todos ... </Text>
+            )}
+
+            <Grid>
+              {todos.length > 0 &&
+                todos.map((todo, index) => (
+                  <GridItem key={todo.id}>
+                    <Todo
+                      id={todo.id}
+                      text={todo.text}
+                      counter={index + 1}
+                      onClick={this.deleteTodo}
+                    />
+                  </GridItem>
+                ))}
+            </Grid>
+          </Container>
+        </Section>
+      </>
+    );
+  }
+}
